7713ea71ce6ba4bed0413a3ff13ec267e7b14ea0
Kandarp Thaker <200424676@student.georgianc.on.ca>
2020-02-26 09:34:46 -0500

    first commit

---
+++ Assignment2/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        [Required]
+        public string FirstName { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        [Required]
+        public string LastName { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        [Required]
+        public string City { get; set; }
+        [Column(TypeName = "nvarchar(40)")]
+        [Required]
+        public string Country { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
---
+++ Assignment2/Models/CustomerContex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2.Models
+{
+    public class CustomerContex : DbContext
+    {
+        public CustomerContex(DbContextOptions<CustomerContex> options) : base(options) 
+        { 
+        
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
